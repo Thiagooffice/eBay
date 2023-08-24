8970aa534c0ae823d7b5925d87aa30a384aecaa7
Thiago <thiagooffice2@gmail.com>
2023-08-23 21:21:42 -0300

    feat: finishing the oerders page

--- app/cart/page.js
+++ app/cart/page.js
@@ -10,13 +10,13 @@ import useIsLoading from "../hooks/useIsLoading"
 import ClientOnly from "../components/ClientOnly"
 
 export default function Cart() {
-    
+
   const router = useRouter()
   const cart = useCart()
 
-  useEffect(() => { 
+  useEffect(() => {
     useIsLoading(true)
-    cart.getCart() 
+    cart.getCart()
     cart.cartTotal()
     useIsLoading(false)
   }, [cart])
@@ -31,51 +31,51 @@ export default function Cart() {
 
   return (
     <>
-        <MainLayout>
-          <div className="max-w-[1200px] mx-auto mb-8 min-h-[300px]">
-            <div className="text-2xl font-bold my-4">Shopping cart</div>
-            <div className="relative flex items-baseline justify-between gap-2">
+      <MainLayout>
+        <div className="max-w-[1200px] mx-auto mb-8 min-h-[300px]">
+          <div className="text-2xl font-bold my-4">Shopping cart</div>
+          <div className="relative flex items-baseline justify-between gap-2">
+            <ClientOnly>
+              <div className="w-[65%]">
+                {cart.getCart().map(product => (
+                  <CartItem key={product.id} product={product} />
+                ))}
+              </div>
+            </ClientOnly>
+
+            <div id="GoToCheckout" className="md:w-[33%] absolute top-0 right-0 m-2">
               <ClientOnly>
-                <div className="w-[65%]">
-                  {cart.getCart().map(product => (
-                    <CartItem key={product.id} product={product}/>
-                  ))}
+                <div className="bg-white p-4 border">
+
+                  <button
+                    onClick={() => goToCheckout()}
+                    className="flex items-center justify-center bg-blue-600 w-full text-white font-semibold p-3 rounded-full mt-4"
+                  >
+                    Go to checkout
+                  </button>
+
+                  <div className="flex items-center justify-between mt-4 text-sm mb-1">
+                    <div>Items ({cart.getCart().length})</div>
+                    <div>${(cart.cartTotal() / 100).toFixed(2)}</div>
+                  </div>
+                  <div className="flex items-center justify-between mb-4 text-sm">
+                    <div>Shipping:</div>
+                    <div>Free</div>
+                  </div>
+
+                  <div className="border-b border-gray-300" />
+
+                  <div className="flex items-center justify-between mt-4 mb-1 text-lg font-semibold">
+                    <div>Subtotal</div>
+                    <div>${(cart.cartTotal() / 100).toFixed(2)}</div>
+                  </div>
                 </div>
               </ClientOnly>
-
-              <div id="GoToCheckout" className="md:w-[33%] absolute top-0 right-0 m-2">
-                  <ClientOnly>
-                      <div className="bg-white p-4 border">
-                          
-                          <button 
-                            onClick={() => goToCheckout()} 
-                            className="flex items-center justify-center bg-blue-600 w-full text-white font-semibold p-3 rounded-full mt-4"
-                          >
-                              Go to checkout
-                          </button>
-
-                          <div className="flex items-center justify-between mt-4 text-sm mb-1">
-                              <div>Items ({cart.getCart().length})</div>
-                              <div>${(cart.cartTotal() / 100).toFixed(2)}</div>
-                          </div>
-                          <div className="flex items-center justify-between mb-4 text-sm">
-                              <div>Shipping:</div>
-                              <div>Free</div>
-                          </div>
-
-                          <div className="border-b border-gray-300"/>
-
-                          <div className="flex items-center justify-between mt-4 mb-1 text-lg font-semibold">
-                              <div>Subtotal</div>
-                              <div>${(cart.cartTotal() / 100).toFixed(2)}</div>
-                          </div>
-                      </div>
-                  </ClientOnly>
-              </div>
             </div>
           </div>
+        </div>
 
-          <SimilarProducts />
+        <SimilarProducts />
 
       </MainLayout>
     </>
